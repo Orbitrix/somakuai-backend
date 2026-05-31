159269871516d61ff5a663828f9f953c5d5eeeca
Habeebamoo <habeebamoo08@gmail.com>
2026-05-31 10:44:27 +0100

    updated subscribe controller

--- src/controllers/subscribeController.ts
+++ src/controllers/subscribeController.ts
@@ -14,7 +14,7 @@ export const subscribeGuest = async (req: Request, res: Response) => {
     // Check if the email is already subscribed
     const existingSubscriber = await Subscriber.findOne({ email });
     if (existingSubscriber) {
-      return res.status(409).json({ success: true, message: 'Email is already subscribed to the waitlist' });
+      return res.status(200).json({ success: true, message: 'Email is already subscribed to the waitlist' });
     }
 
     const newSubscriber = new Subscriber({ email });
@@ -22,9 +22,9 @@ export const subscribeGuest = async (req: Request, res: Response) => {
 
     //send email notification
 
-    res.status(200).json({ success: true, message: 'Successfully subscribed to the waitlist!' });
+    return res.status(200).json({ success: true, message: 'Successfully subscribed to the waitlist!' });
   } catch (error) {
     console.error('Error subscribing guest:', error);
-    res.status(500).json({ success: false, message: 'Internal server error' });
+    return res.status(500).json({ success: false, message: 'Internal server error' });
   }
 };
